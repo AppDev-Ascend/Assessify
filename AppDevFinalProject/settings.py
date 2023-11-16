82d1113a2570be683294802ad519410b97f99736
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-11-17 00:17:06 +0800

    Create App named 'api'

--- AppDevFinalProject/settings.py
+++ AppDevFinalProject/settings.py
@@ -27,6 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000', # React App
+    'http://localhost:8000', # Django App
+]
+CORS_ALLOWED_CREDENTIALS = True
+
 
 # Application definition
 
@@ -37,9 +43,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api.apps.ApiConfig'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
