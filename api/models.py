8b672181e574a1876cb46367e9050efe57c0bc2d
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-11-17 21:47:03 +0800

    Add Models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# User
+class User(AbstractUser):
+    display_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    student_id = models.IntegerField()
+
+
+class Teacher(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    teacher_id = models.IntegerField()
+
+# Educational System
+class Topic(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField
