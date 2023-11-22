cc26ae14d88783519769e94e680499a613c81fd4
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-11-22 16:19:46 +0800

    update models

--- api/models.py
+++ api/models.py
@@ -19,5 +19,34 @@ class Teacher(models.Model):
 
 # Educational System
 class Topic(models.Model):
-    id = models.AutoField(primary_key=True)
     name = models.CharField
+
+class Lesson(models.Model):
+   name = models.CharField(max_length=255)
+   link = models.URLField()
+   topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+
+class Section(models.Model):
+   section_no = models.IntegerField()
+   no_of_questions = models.IntegerField()
+   lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+
+class AssessmentType(models.Model):
+    name = models.CharField(max_length=255)
+
+class Assessment(models.Model):
+    assessment_type = models.ForeignKey(AssessmentType, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+
+
+class Question(models.Model):
+    question_no = models.IntegerField()
+    question = models.TextField()
+    assessment = models.ForeignKey(Assessment, on_delete=models.CASCADE)
+
+class Response(models.Model):
+    user = models.ForeignKey(Student, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    score = models.IntegerField()
+   
